2e11a45169aa3a206a623e25aa29d2c1ab2ba417
rglepe <raul.garcia@factoriaf5.org>
2026-05-13 16:20:55 +0200

    feat: integrate ButtonTrash component into CustomerItem template

--- src/app/features/customer-item/customer-item.ts
+++ src/app/features/customer-item/customer-item.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { ICustomer } from '../interfaces/customer.interface';
+import { ButtonTrash } from "../../shared/ui/button-trash/button-trash";
 
 @Component({
   selector: 'app-customer-item',
-  imports: [],
+  imports: [ButtonTrash],
   templateUrl: './customer-item.html',
   styleUrl: './customer-item.css',
 })
