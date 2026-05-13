aee4fb0e579eddd7035f362ec0d3a0c4cdc1b101
rglepe <raul.garcia@factoriaf5.org>
2026-05-13 16:20:38 +0200

    feat: add ButtonTrash component with HTML, CSS, and test files

---
+++ src/app/shared/ui/button-trash/button-trash.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-trash',
+  imports: [],
+  templateUrl: './button-trash.html',
+  styleUrl: './button-trash.css',
+})
+export class ButtonTrash {}
